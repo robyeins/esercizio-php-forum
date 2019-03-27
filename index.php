100240879d7d82abf12fe577c5dbda367f3fc50e
unknown <andreabordignon50@gmail.com>
2019-03-27 12:25:33 +0100

    update

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 
-<?php
+<?php 
 
+$nome = (isset($_POST["nome"])) ? $_POST["nome"] : "";
+$cognome = (isset($_POST["cognome"])) ? $_POST["cognome"] : "";
+$indirizzo = (isset($POST["indirizzo"])) ? $_POST["indirizzo"] : "";
 
-    		$nome = (isset($_POST["exampleInputEmail1"])) ? $_POST["exampleInputEmail1"] : "";
- 					   
-
-            print $nome;
-
-?>    
-
-
-
+?>
 
 
 <!doctype html>
@@ -23,46 +18,36 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
+
+
     <title>Hello, world!</title>
   </head>
   <body>
-  	<div class="container">
-  		<div class="alert alert-primary" role="alert">
-  			<?php
-  			print $nome . "<br>";
-  			print $cognome . "<br>";
-  			print $indirizzo . "<br>";
-  			?>
-  	
-
-
-
-
+    <H1>✌️</H1>
     <div class="container">
-  <form action="index.php" method="post">
-  <div class="form-group">
-    <label for="exampleInputEmail1">Nome</label>
-    <input type="email" name="exampleInputEmail1" class="form-control" id="exampleInputEmail1" aria-describedby="nomeHelp" placeholder="Inserisci nome">
-  </div>
-   <div class="form-group">
-    <label for="exampleInputEmail1">Cognome</label>
-    <input type="email" name="exampleInputEmail1" class="form-control" id="exampleInputEmail1" aria-describedby="cognomeHelp" placeholder="Inserisci cognome">
-    
-  </div>
-  
-  
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">Indirizzo</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
-   <small id="emailHelp" class="form-text text-muted">Deve contenere la via, il numero civico e il cap</small>
-  <button type="submit" class="btn btn-primary">Invia</button>
- 
-</form>
+      <div class="alert alert-primary" role="alert"
+      	<?php
+	  	  foreach ($_POST as $key => $value) {
+	  	  	print $value;
+	  	  }
+	  	?>
+    <form action="index.php" method="post">
+    <div class="form-group">
+      <label for="nome">Nome</label>
+      <input type="email" name="nome" class="form-control" id="nome" aria-describedby="nomeHelp" placeholder="Inserisci il tuo nome">
     </div>
-    
-
-
+    <div class="form-group">
+      <label for="cognome">Cognome</label>
+      <input type="email" name="cognome" class="form-control" id="cognome" aria-describedby="cognomeHelp" placeholder="Inserisci il tuo cognome">
+    </div>
+     <div class="form-group">
+    <label for="indirizzo">Indirizzo</label>
+    <textarea name="indirizzo" class="form-control" id="indirizzo" rows="3"></textarea>
+  </div>
+  <small id="emailHelp" class="form-text text-muted">indirizzo di casa e numero civico</small>
+    <button type="submit" class="btn btn-primary">Invia</button>
+  </form>
+    </div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="js/jquery-3.3.1.min.js"></script>
